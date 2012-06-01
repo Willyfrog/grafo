838c0586e17ceac8af93727197ccc026f65640bc
Guillermo Vayá <gvaya@arplatia.com>
2012-06-01 12:14:25 +0200

    añadida scaladoc a nodo

--- common/src/main/scala/grafo/Nodo.scala
+++ common/src/main/scala/grafo/Nodo.scala
@@ -5,19 +5,36 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import math.{sqrt, pow}
 
 /**
- * Created with IntelliJ IDEA.
- * User: guillermo
- * Date: 25/05/12
- * Time: 9:51
- * To change this template use File | Settings | File Templates.
+ * @author guillermo
+ * @since 25/05/12
+ */
+
+/**
+ * Clase Nodo
+ * @define THIS Nodo
+ * @define Parent Ninguno
+ *
+ * @param x Posicion en el eje x
+ * @param y Posicion en el eje y
  */
 
 class Nodo(val x: Float, val y: Float) {
+
   val z = 0
 
+  /**
+   * Coordenadas
+   * @return Las coordenadas x,y,z en formato de lista de floats
+   */
   def coordenadas(): List[Float] = {
     return List(x, y, z)
   }
 
+  /**
+   * Distancia del nodo a un punto dado
+   * @param x1 coordenada x del punto
+   * @param y1 coordenada y del punto
+   * @return distancia al punto. Siempre positiva
+   */
   def distance(x1:Float,y1:Float): Double = sqrt(pow(x1-x,2)+pow(y1-y,2))
 }
